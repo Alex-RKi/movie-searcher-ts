17fcf118698f48fe7f1afd48d26864046e310ab4
Alex RKi <aleksandr.ksj@mail.ru>
2020-11-14 22:25:19 +0300

    removed data interfaces in separate folder - src/interfaces

---
+++ src/interfaces/index.ts
@@ -0,0 +1,3 @@
+import { movieInterface } from "./movie-interface"; // Provider data
+import { MDBServiceInterface } from "./mdb-service-interface"; // Context
+export type { movieInterface, MDBServiceInterface };
---
+++ src/interfaces/mdb-service-interface.ts
@@ -0,0 +1,7 @@
+interface MDBServiceInterface {
+  id: number;
+  favoritsList: any;
+  popularsList: any;
+  page: number;
+}
+export type {MDBServiceInterface}
