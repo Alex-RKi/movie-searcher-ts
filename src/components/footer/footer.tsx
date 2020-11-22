e34e8f63d8e996a61fa7f9161f613ef29629a513
Alex RKi <aleksandr.ksj@mail.ru>
2020-11-22 19:32:54 +0300

    Added scroll to top btn to footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
 const Footer = () => {
+  const scrollToTop = () => {
+    window.scrollTo({top: 0, left: 0, behavior: 'smooth'});
+
+  }
+
   return (
     <footer id='footer' className='jumbotron w-100'>
+      <button className='w-100 btn btn-outline-secondary' onClick={scrollToTop}>Scroll to top</button>
       <hr></hr>
     <span>Data and images provided by themoviedb.org</span>
     <span></span>
