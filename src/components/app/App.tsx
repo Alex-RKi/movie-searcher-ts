a941b4bf3f97e71c7d207ce92f4a15ce2ef51604
Alex RKi <aleksandr.ksj@mail.ru>
2020-11-23 18:54:35 +0300

    Rewrite state from Reacte hooks to Redux useSelector

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -6,13 +6,13 @@ import {
   SaveSearchResults,
   GetPopulars,
   SaveSearchText,
-  savePage,
-  saveLocation
+  SavePage,
+  SaveLocation
 } from '../../actions'
 import { RootStateType } from '../../reducers';
 import MovieDBService from '../../services';
 import { MDBServiceContext, MDBServiceInterface } from '../movie-db-service-provider';
-import { movieInterface } from '../../interfaces';
+import { movieInterface, ResponseInterface } from '../../interfaces';
 import Footer from '../footer';
 import Header from '../header';
 import CardList from '../cards-list';
@@ -20,51 +20,30 @@ import { MovieDescription } from '../pages'
 import ErrorBoundry from '../error_boundry';
 import Spinner from '../spinner';
 
-interface ResponseInterface {
-  readonly page: number,
-  readonly results: [],
-  readonly total_pages: number,
-  readonly total_results: number
-}
-type searchLangType = "en-US" | "ru-RU";
-
 
 const App = () => {
 
   const movieDBService = new MovieDBService();
   const dispatch = useDispatch();
-  const initial: any = [];
 
-  const { popularsList } = useSelector((state: RootStateType) => state.popularsList)
-  console.log(popularsList);
-  console.log('-----popularsList----');
-
-  const [favoritsList, setFavorited] = useState(initial);
-  const [searchResults, saveSearchResults] = useState(initial);
-  const [searchText, saveSearchText] = useState('');
-  const [page, setPage] = useState(1);
-  const [location, setLocation] = useState('/');
-  console.log('page now ' + page);
-  console.log('text is ' + searchText);
-  const trackLocation = (newLocation: string) => {
-    setLocation(newLocation);
-
-  }
+  const { popularsList } = useSelector((state: RootStateType) => state.popularsList);
+  const { favoritsList } = useSelector((state: RootStateType) => state.favoritsList);
+  const { searchResults } = useSelector((state: RootStateType) => state.searchResults);
+  const { searchText } = useSelector((state: RootStateType) => state.searchText);
+  const { page } = useSelector((state: RootStateType) => state.page);
+  const { location } = useSelector((state: RootStateType) => state.location);
 
   useEffect(() => {
     movieDBService.getPopular()
       .then((response: ResponseInterface) => {
         const popularsList: movieInterface[] = response.results;
-        //savePopulars(popularsList);
         dispatch(GetPopulars(popularsList))
       });
   }, []);
-  useEffect((): any => {
+  useEffect(() => {
     document.addEventListener('scroll', trackScrolling);
     let cancelled = false;
-    console.log('location is ' + location)
     if (!cancelled && location === '/') {
-      console.log(searchText, page);
       searchMovies(searchText, page);
     }
     return () => {
@@ -73,21 +52,23 @@ const App = () => {
     }
   }, [searchText, page, location]);
 
-  const isBottom = (el: any) => {
+  const isBottom = (el: HTMLElement) => {
     return el.getBoundingClientRect().bottom <= window.innerHeight;
   };
-
+  const trackLocation = (newLocation: string) => {
+    dispatch(SaveLocation(newLocation))
+  }
   const getSearchText = (text: string) => {
-    saveSearchResults([]);
-    setPage(1);
-    saveSearchText(text);
+    dispatch(SaveSearchResults([]));
+    dispatch(SavePage(1));
+    dispatch(SaveSearchText(text));
   }
   const trackScrolling = () => {
-    const wrappedElement = document.getElementById('footer');
+    const wrappedElement: any = document.getElementById('footer');
     if (isBottom(wrappedElement)) {
       document.removeEventListener('scroll', trackScrolling);
       const nextPage = page + 1;
-      setPage(nextPage);
+      dispatch(SavePage(nextPage));
     }
   };
   const addToList = (data: object, arr: Array<object> | []): Array<object> => {
@@ -102,16 +83,16 @@ const App = () => {
   }
   const toggleFavorit: Function = (movieData: movieInterface) => {
     const { id } = movieData
-    favoritsList.findIndex((movie: movieInterface) => movie.id === id)
-      > -1 ?
-      setFavorited(removeFromList(id, favoritsList)) :
-      setFavorited(addToList(movieData, favoritsList));
+    favoritsList.some((elem: movieInterface) => elem.id === id) ?
+      dispatch(UpdateFavorits(removeFromList(id, favoritsList))) :
+      dispatch(UpdateFavorits(addToList(movieData, favoritsList)));
   }
   const searchMovies: Function = (query: string, page: number = 1) => {
     if (query.length < 1) {
       return;
-    }
+    };
     let inquire = query.toLowerCase();
+    type searchLangType = "en-US" | "ru-RU";
     let lang: searchLangType = "en-US";
     if (/^[а-яА-Я]+$/.test(inquire)) {
       lang = "ru-RU";
@@ -121,7 +102,7 @@ const App = () => {
       .then((response: ResponseInterface) => {
         if (response.results.length < 1) return;
         const newResult = filterResults(searchResults, response.results)
-        saveSearchResults(newResult);
+        dispatch(SaveSearchResults(newResult));
       });
   };
   const filterResults = (prevRes: any, newRes: any) => {
@@ -130,14 +111,10 @@ const App = () => {
   }
   const MDBSProviderData: MDBServiceInterface = {
     favoritsList,
-    popularsList,
-    page: 3,
-    toggleFavorit,
-    searchMovies,
-    searchResults: [],
-    recommededList: [],
+    toggleFavorit
   }
   if (!popularsList) return <Spinner />
+
   return (
     <ErrorBoundry>
       <MDBServiceContext.Provider value={MDBSProviderData}>
@@ -149,13 +126,13 @@ const App = () => {
             />
             <Switch>
               <Route path='/' exact
-                render={(): any => <CardList list={searchText ? searchResults : popularsList} />}
+                render={(): JSX.Element => <CardList list={searchText ? searchResults : popularsList} />}
               />
               <Route path='/favorits'
                 render={() => <CardList list={favoritsList} />}
               />
               <Route path='/description/:id'
-                render={({ match }): any => {
+                render={({ match }): JSX.Element => {
                   const { id } = match.params;
                   return <MovieDescription id={id} />
                 }}
